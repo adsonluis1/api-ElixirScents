60e22326a8bdd23979295d48884ad824f9771d8d
adsonluis1 <dev.adsonluis@gmail.com>
2024-09-02 22:30:08 -0300

    controllers clients

---
+++ controllers/Clients.js
@@ -0,0 +1,91 @@
+const bcrypt = require('bcrypt')
+const ClientsModels = require('../models/Clients')
+const ProductsModels = require('../models/Products')
+
+module.exports = class ClientControllers{
+    static async addAccount (req,res){
+        const {name,password,email} = req.body
+        const saltRounds = 10
+        try {
+            const user = await ClientsModels.login(email)
+            if(user?.email == email){
+                res.status(400).json({message:"already registered user"})
+                return
+            }
+            const hashedPassword = await bcrypt.hashSync(password,saltRounds)
+            const use = new ClientsModels(name,email,hashedPassword)
+            await use.save()
+            res.status(201).json({message:'ok'})
+        } catch (error) {
+            res.status(400).json({error:error.message})
+        }
+    }
+
+    static async login (req,res){
+        const {email,password} = req.body
+        try {
+            const user = await ClientsModels.login(email)
+            console.log(user)
+            
+            if(!user){
+                res.status(400).json({message:"User not found"})
+                return
+            }
+            const done = await bcrypt.compare(password, user.password)
+            if(done){
+                res.json({message:user})
+            }else{
+                res.status(400).json({message:'Incorrect password'})
+            }
+        } catch (error) {
+            res.status(400).json({error:error.message})
+        }
+    }
+
+    static async addAdditionalInfo(req, res){
+       const {street, cep, city, number, cpf, account} = req.body
+       const address = {
+            street,
+            cep,
+            city,
+            number
+       }
+       
+       try {
+            await ClientsModels.addAdditionalInfo(account._id,address,cpf)
+            res.json({message:"Changed information"})
+       } catch (error) {
+            res.status(400).json({error:error.message})
+       }
+
+    }
+
+    static async buy (req, res){
+        const {product, user,amount} = req.body
+        product.amount = amount
+        if(user.cpf == null){
+            res.status(400).json({message:"You cannot buy without registering your CPF"})
+            return
+        }
+
+        if(user.address != null){
+            res.status(400).json({message:"You can't buy without the address"})
+            return
+        }
+
+        if(product.amount <= 0){
+            res.status(400).json({message:"We don't have stock"})
+            return
+        }
+
+        try {
+            await ClientsModels.buy(user._id,product)
+            await ProductsModels.bought(product.label,product._id)
+            res.json({message:"Product bougth"})
+        } catch (error) {
+            res.json({error:error.message})
+        }
+
+    }  
+
+}
