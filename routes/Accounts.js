07bd7606ece0ab3382881642c7062e11466407c0
adsonluis1 <dev.adsonluis@gmail.com>
2024-09-11 15:38:17 -0300

    rota de delete virou patch

--- routes/Accounts.js
+++ routes/Accounts.js
@@ -2,11 +2,12 @@ const express = require('express')
 const routes = express.Router()
 const ClientsControllers = require('../controllers/Clients')
 
+routes.get('/getBag/:idClient', ClientsControllers.getProducIntoBag)
 routes.post('/create', ClientsControllers.addAccount)
 routes.post('/login', ClientsControllers.login)
 routes.post('/addProductIntoBag', ClientsControllers.addProducIntoBag)
 routes.post('/buy', ClientsControllers.buy)
-routes.delete('/removeProductIntoBag/:idClient/:idProduct', ClientsControllers.removeProducIntoBag)
+routes.patch('/removeProductIntoBag/:idClient/', ClientsControllers.removeProducIntoBag)
 routes.patch('/addAdditionalInfo', ClientsControllers.addAdditionalInfo)
 
 module.exports = routes
