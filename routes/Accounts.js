1f76906baa7226b9a3821572f7adba8a15697ddf
adsonluis1 <dev.adsonluis@gmail.com>
2024-09-18 12:35:14 -0300

    rota de pesquisa de produto

--- routes/Accounts.js
+++ routes/Accounts.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const routes = express.Router()
 const ClientsControllers = require('../controllers/Clients')
+const ProductsControllers = require('../controllers/Products')
 
 routes.get('/getHistory/:idClient', ClientsControllers.getPurchaseHistory)
 routes.get('/getBag/:idClient', ClientsControllers.getProducIntoBag)
+routes.get('/search/:product', ProductsControllers.searchProcuts)
 routes.post('/create', ClientsControllers.addAccount)
 routes.post('/login', ClientsControllers.login)
 routes.post('/addProductIntoBag', ClientsControllers.addProducIntoBag)
